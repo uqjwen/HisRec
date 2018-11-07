aef85957f8d09f047e250dbb2e7b8983a786b96f
uqjwen <wen_jiahui@outlook.com>
2018-11-07 13:55:27 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -26,26 +26,47 @@ def precision_k_curve(y_true,y_pred,max_k):
 		res.append(step/(k+1))
 	return res 
 
-def ndcg(y_true, y_pred):
-	dcg = 0.0
-	# recommend = np.argsort(-y_pred)
-	for i,item in enumerate(y_true):
-		if item==1:
-			dcg += 1/(np.log(i+2)/np.log(2))
-	idcg = 0.0
-	for i in range(min(len(y_pred), len(y_true))):
-		idcg += 1/(np.log(i+2)/np.log(2))
+# def ndcg(y_true, y_pred):
+# 	dcg = 0.0
+# 	# recommend = np.argsort(-y_pred)
+# 	for i,item in enumerate(y_true):
+# 		if item==1:
+# 			dcg += 1/(np.log(i+2)/np.log(2))
+# 	idcg = 0.0
+# 	for i in range(min(len(y_pred), len(y_true))):
+# 		idcg += 1/(np.log(i+2)/np.log(2))
 
-	ndcg = dcg/idcg
+# 	ndcg = dcg/idcg
 
-	return ndcg
-def ndcg_k_curve(y_true, y_pred, max_k):
-	res = []
-	recommend = np.argsort(-y_pred)
+# 	return ndcg
+# def ndcg_k_curve(y_true, y_pred, max_k):
+# 	res = []
+# 	recommend = np.argsort(-y_pred)
 
-	for i in range(max_k):
-		res.append(ndcg(y_true[recommend][:i+1], y_pred[recommend][:i+1]))
-	return res
+# 	for i in range(max_k):
+# 		res.append(ndcg(y_true[recommend][:i+1], y_pred[recommend][:i+1]))
+# 	return res
+def dcg_score(y_true, y_score, k=5):
+  order = np.argsort(y_score)[::-1]
+  y_true = np.take(y_true, order[:k])
+  gain = 2**y_true-1
+  discounts = np.log2(np.arange(len(y_true))+2)
+
+  ##############idcg
+  # idcg = sorted(y_true, reverse = True)[:k]
+  # idcg = 2**idcg-1
+  # idcg = np.sum(idcg/discounts)
+  # return np.sum(gain/discounts)/idcg
+
+
+
+  return np.sum(gain/discounts)
+
+def ndcg_k_curve(y_true, y_score, k_max):
+  res = []
+  for k in range(k_max):
+    res.append(dcg_score(y_true, y_score, k+1))
+  return res
 	
 
 
